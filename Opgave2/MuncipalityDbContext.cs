50281f5650b80271cd885022173fad45e6ef3687
skragh <83223908+skragh@users.noreply.github.com>
2021-11-11 12:44:50 +0100

    updated packages

---
+++ Opgave2/MuncipalityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave2
+{
+    class MuncipalityDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+        }
+    }
+}
