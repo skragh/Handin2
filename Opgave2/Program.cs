a1d50bf33c51734ded86f88c8f05d97e9970c830
skragh <83223908+skragh@users.noreply.github.com>
2021-11-11 12:31:05 +0100

    Add project files.

---
+++ Opgave2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opgave2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
