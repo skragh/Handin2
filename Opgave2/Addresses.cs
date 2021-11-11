b8f5715b9f226e390a59e0121842a7dc6e35eeb4
HackTheOxidation <tomas.hagenau@protonmail.ch>
2021-11-11 12:55:12 +0100

    Implemented Addresses

---
+++ Opgave2/Addresses.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Opgave2
+{
+    public class Addresses
+    {
+        [Key]
+        public int AddressId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string street { get; set; }
+
+        [Required]
+        public int number { get; set; }
+
+        [Required]
+        [MaxLength(4)]
+        [MinLength(4)]
+        public int postalCode { get; set; }
+    }
+}
