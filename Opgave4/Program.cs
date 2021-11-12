b07be33e81aec90ab19117a09b6bd8532cf08e5a
HackTheOxidation <tomas.hagenau@protonmail.ch>
2021-11-12 12:00:45 +0100

    Added queries for opgave 4

--- Opgave4/Program.cs
+++ Opgave4/Program.cs
@@ -17,17 +17,35 @@ namespace Opgave4
             else
                 Console.WriteLine("Test is null");
 
+
             var query = new Queries(context);
 
+            Console.WriteLine("\n--- Gets all Rooms in Municipality (Example: 8300 Odder) ---\n");
             var roomsInOdder = query.GetRoomsInMunicipality(8300);
             if (roomsInOdder != null)
                 foreach (var room in roomsInOdder)
                     Console.WriteLine($"{room}");
 
+
+            Console.WriteLine("\n--- Gets all Societies by Activity (Example: Computing) ---\n");
             var allSocieties = query.GetSocietiesByActivity("Computing");
             if (allSocieties != null)
                 foreach (var (society, member, address) in allSocieties)
                     Console.WriteLine($"{society}, {member.person}, {address}");
+
+
+            Console.WriteLine("\n--- Gets all Booked Rooms ---\n");
+            var allBookedRooms = query.GetAllBookedRooms();
+            if (allBookedRooms != null)
+                foreach (var (room, location, person, timespan) in allBookedRooms)
+                    Console.WriteLine($"{room}, {location}, {person}, {timespan}");
+
+
+            Console.WriteLine("\n--- Gets all Future Bookings with Access key for a Key Responsible (Example: KeyResponsibleId = 1) ---\n");
+            var allFutureBookings = query.GetFutureBookingsWithAccessKey(1);
+            if (allFutureBookings != null)
+                foreach (var (booking, accessKey) in allFutureBookings)
+                    Console.WriteLine("{booking}, {accessKey}");
         }
     }
 }
