da2769c8565e6aa3f33057c17b584099a9e1a031
HackTheOxidation <tomas.hagenau@protonmail.ch>
2021-11-12 11:07:28 +0100

    Completed queries for opgave2

--- Opgave2/Program.cs
+++ Opgave2/Program.cs
@@ -14,8 +14,20 @@ namespace Opgave2
 
             if (test != null)
                 Console.WriteLine($"{test.ToString()}");
-            else 
+            else
                 Console.WriteLine("Test is null");
+
+            var query = new Queries(context);
+
+            var roomsInOdder = query.GetRoomsInMunicipality(8300);
+            if (roomsInOdder != null)
+                foreach (var room in roomsInOdder)
+                    Console.WriteLine($"{room}");
+
+            var allSocieties = query.GetSocietiesByActivity("Computing");
+            if (allSocieties != null)
+                foreach (var (society, member, address) in allSocieties)
+                    Console.WriteLine($"{society}, {member.person}, {address}");
         }
     }
 }
